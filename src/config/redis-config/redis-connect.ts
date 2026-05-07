ff8c7abc2af4d1f50442a08608ee2e9504395895
jettechnologies <tomesitime04@gmail.com>
2026-05-07 10:08:03 -0700

    chore: updated the format for declaring a new instance of redis

--- src/config/redis-config/redis-connect.ts
+++ src/config/redis-config/redis-connect.ts
@@ -1,5 +1,20 @@
 import { Redis } from "ioredis";
 
-export const redis = new Redis(process.env.REDIS_URL!, {
+const HOST = process.env.REDIS_HOST!;
+const PORT = process.env.REDIS_PORT! as unknown as number;
+const USERNAME = process.env.REDIS_USERNAME!;
+const PASSWORD = process.env.REDIS_PASSWORD!;
+
+export const redis = new Redis({
+  host: HOST,
+  port: PORT,
+  username: USERNAME,
+  password: PASSWORD,
   maxRetriesPerRequest: null,
 });
+
+// import { Redis } from "ioredis";
+
+// export const redis = new Redis(process.env.REDIS_URL!, {
+//   maxRetriesPerRequest: null,
+// });
